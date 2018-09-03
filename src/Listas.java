bb2b34825e7e151d68098a9d039a88ea30af7bc4
Alisson Youssf <youssfbr@gmail.com>
2018-09-03 14:08:31 -0300

    Instanciando a lista

--- src/Listas.java
+++ src/Listas.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
 
+/*
+ * https://docs.oracle.com/javase/10/docs/api/java/util/List.html
+ * Alisson Youssf - Revisão emm 03/09/2018. 
+ */
+ 
 public class Listas {
 
 	public static void main(String[] args) {
 		
+		List<String> list = new ArrayList<>(); // Uma interface só pode ser instaciada com uma classe do tipo ArrayList, LinkedList...
 	}
-
 }
