ba757ce8fd750ffab610f8974aa84c9976389a7e
Alisson Youssf <youssfbr@gmail.com>
2018-09-03 15:16:09 -0300

    Deixando na lista apenas quem começa com a letra M

--- src/Listas.java
+++ src/Listas.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
  * https://docs.oracle.com/javase/10/docs/api/java/util/List.html
@@ -40,5 +41,13 @@ public class Listas {
 		
 		System.out.println("Index of Chico Buarque: " + list.indexOf("Chico Buarque")); // Encontra a posição de um elemento
 		System.out.println("Index of Djavan: " + list.indexOf("Djavan")); // Caso não encontre retorna -1
+		
+		System.out.println("------------------------------------------");
+		
+		//Deixar na lista apenas quem comece com a letra M
+		List<String> result = list.stream().filter(x -> x.charAt(0) == 'M').collect(Collectors.toList()); //Declaro nova lista, Converto pra stream, faço a operação lambada que quero e depois retorno pra lista.
+		for (String x : result) {
+			System.out.println(x);
+		}
 	}
 }
