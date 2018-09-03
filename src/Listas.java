1c8b8d774bd6f33f92147526aec4a9a03d283b32
Alisson Youssf <youssfbr@gmail.com>
2018-09-03 14:11:50 -0300

    Adicionando elementos à lista

--- src/Listas.java
+++ src/Listas.java
@@ -11,5 +11,10 @@ public class Listas {
 	public static void main(String[] args) {
 		
 		List<String> list = new ArrayList<>(); // Uma interface só pode ser instaciada com uma classe do tipo ArrayList, LinkedList...
+		
+		list.add("Maria Bethania");
+		list.add("Gal Costa");
+		list.add("Gilberto Gil");
+		list.add("Caetano Veloso");
 	}
 }
